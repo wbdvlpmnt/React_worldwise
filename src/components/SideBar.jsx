4f21700bd20f8c952a6ef5cb72637241209c8968
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-23 10:33:57 -0600

    building app layout

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+export default function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Co.
+        </p>
+      </footer>
+    </div>
+  );
+}
