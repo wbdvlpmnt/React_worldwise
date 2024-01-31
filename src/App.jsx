5285d3f39f513e592f94b831462a43212df69090
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-31 10:33:18 -0600

    dynamic routes with use params

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 export default function App() {
@@ -44,13 +45,14 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           <Route path="form" element={<p>form</p>} />
         </Route>
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
