f93f42180b61226cdad46998a1b551935e76fdc7
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-02-05 09:33:09 -0600

    getting state from context

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,11 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CityList({ cities, isLoading }) {
+export default function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) {
     return <Spinner />;
   }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 const citiesContext = createContext();
@@ -34,4 +34,13 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+// cusotm hook to consume data
+function useCities() {
+  const context = useContext(citiesContext);
+  if (context === undefined) {
+    throw new Error("Cities context is used outside of the cities provider.");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
